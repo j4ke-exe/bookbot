90fab727ae5beff287137eb183e3dfbb20a90f08
Jake Garrison <101604552+j4ke-exe@users.noreply.github.com>
2025-04-06 16:54:58 -0600

    corrected main

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ def main(get_book_text):
    with open("/books/frakenstein.txt") as f:
       file_contents = f.read()
       print(file_contents)
-   
-main()
+
+if __name__ == "__main__":
+   main()
